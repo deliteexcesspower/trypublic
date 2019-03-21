35d8228dd79a4cf65b4d41b4a05a9e15f8f17040
smosley <smosley@uoregon.edu>
2019-03-21 09:23:08 -0700

    :stew: automated-update 2019-03-21 09:22:58.700000

---
+++ lab2019/composite-validation/demo_composite_validation_001a.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+'''
+### <beg-file_info>
+### document_metadata:
+###   - caption: "demo_composite_validation_001a"
+###     dmid: "uu182upon_reproach"
+###     date: created="2019-03-21 09:21:04"
+###     last: lastmod="2019-03-21 09:21:04"
+###     tags: __tags__
+###     author:     created="__author__"
+###     filetype:   "__filetype__"
+###     lastupdate: "__lastupdate__"
+###     desc: |
+###         * demonstrate the use of composite validation in python
+###         * DEPENDENCY -- jmespath query language package
+###         * DEPENDENCY -- cerberus validation package
+###     seealso: |
+###         * __seealso__
+###     seeinstead: |
+###         * __seeinstead__
+### <end-file_info>
+'''
+
+## --------------------------------------
+## begin_:
+
+if('init_python'):
+  import os
+  import sys
+  pass
+  import cerberus
+  import codecs
+  import yaml
+  import textwrap
+  import jmespath
+  import pprint
+  pass
+##endif
+
+if(True and "sample documents"):
+  aadocuments = []
+  aadocuments.append(yaml.safe_load('''
+      person_fname:       homer
+      person_lname:       himpson
+      person_age:         1
+      prize_caption:      free beer for life
+      prize_email:        prizes@abcbooze.com
+      prize_category:     alchohol
+    '''))
+  aadocuments.append(yaml.safe_load('''
+      person_fname:       jomer
+      person_lname:       jimpson
+    '''))
+  aadocuments.append(yaml.safe_load('''
+      person_fname:       helen
+      person_lname:       himpson
+      person_age:         16
+      prize_caption:      free ammo for life
+      prize_email:        prizes@zzzguns.com
+      prize_category:     firearms
+    '''))
+  aadocuments.append(yaml.safe_load('''
+    person_fname:         maggie
+    person_lname:         himpson
+    person_age:           3
+    prize_caption:        free puppy
+    prize_email:          prizes@123pets.com
+    prize_category:       pets
+    parent_name:          homer himpson
+    '''))
+  pass
+##endif
+
+
+
---
+++ lab2019/composite-validation/readme.txt
@@ -0,0 +1,16 @@
+### <beg-file_info>
+### document_metadata:
+###   - caption: "composite-validation"
+###     dmid: "uu099strinicrif"
+###     date: created="2019-03-21T09:20:10"
+###     last: lastmod="2019-03-21T09:20:10"
+###     tags: validation,howto,
+###     author: created="__author__"
+###     lastupdate: "__lastupdate__"
+###     desc: |
+###         * general purpose validation of structured data
+###     seealso: |
+###         * __seealso__
+###     seeinstead: |
+###         * __seeinstead__
+### <end-file_info>

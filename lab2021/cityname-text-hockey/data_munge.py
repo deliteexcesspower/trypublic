2f777ce8b51c5c6ce76e256943e39d3d6669ef13
smosley <smosley@uoregon.edu>
2021-05-30 19:58:57 -0700

    munge-update-seent
    munge-update-seent
    :eggplant: automated update ;; 20210530:195855
    
    	cityname_hershey.yaml
    	cityname_raw.txt
    	data_munge.py
    	data_munge.yaml

---
+++ lab2021/cityname-text-hockey/data_munge.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+'''
+### <beg-file_info>
+### document_metadata:
+###   - caption: "caption"
+###     dmid: "uu674cradlingaycustodian"
+###     date: created="2021-05-30 19:26:23"
+###     last: lastmod="2021-05-30 19:26:23"
+###     tags:       python,munge,codegen,city,world,places
+###     people:
+###         - __people__
+###     author:     created="__author__"
+###     filetype:   "__filetype__"
+###     lastupdate: "__lastupdate__"
+###     namespace:
+###         - nams: __namespace__
+###     desc: |
+###         ## Overview
+###         * __desc__
+###     linktop:
+###         -
+###     seealso: |
+###         ## See also
+###         * __seealso__
+###     seeinstead: |
+###         * __seeinstead__
+### <end-file_info>
+'''
+
+## -------------------------------------------------------------------
+## begin_
+
+## <xrbeg id="uu129swasp1622427003aa" d="python.init">##
+if(True):
+  import os,sys
+  pass
+  import codecs
+  import pprint
+  import re
+  import yaml
+  pass
+  # import codecs
+  # import textwrap
+## <xrend>##uu129swasp1622427003aa
+
+## -------------------------------------------------------------------
+## begin_
+
+## <xrbeg id="uu129swasp1622427005aa" d="nameismain">##
+if(__name__ == '__main__'):
+
+  ## <xrbeg id="uu143tinkz1622428" d="file_munage">##
+  if(True):
+
+    sg711href = './city-name-raw.txt'
+    mytext = codecs.open(sg711href, 'rb', encoding='utf8').read()
+    mylines = [vxx for vxx in mytext.splitlines() if (vxx.strip())]
+    ag6964trigoutt = []
+    pass
+    for thisline in mylines:
+      ddtemp = dict()
+      ddtemp['line']        =   thisline
+      ddtemp['len_name']    =   '{vxx:03d}'.format(vxx=thisline.__len__())
+      ddtemp['len_space']   =   '{vxx:03d}'.format(vxx=re.findall(r'[\x20]+',thisline).__len__())
+      ddtemp['len_nonasc']  =   '{vxx:03d}'.format(vxx=re.findall(r'[^\x00-\x7F]+',thisline).__len__())
+      pass
+      # sg98cranoutt = ''' {{  cityname: {line} , city_len: '{len}' , city_spa: '{has_space}' }}'''.format(**ddtemp)
+      # dd99cranoutt = yaml.safe_load(sg98cranoutt)
+      # print(sg98cranoutt)
+      # ag6964trigoutt.append(sg98cranoutt)
+      ag6964trigoutt.append(ddtemp)
+    pass
+    # pprint.pprint(ag6964trigoutt)
+    vg2520skufoutt = yaml.safe_load('''
+      droot: {}
+      troot: {}
+      ''')
+    vg2520skufoutt['droot']['cityname_seent_table'] = ag6964trigoutt
+    print(yaml.safe_dump(vg2520skufoutt))
+    # print(vg2520skufoutt)
+  ## <xrend>##uu143tinkz1622428
+
+  pass
+## <xrend>##uu129swasp1622427005aa
+

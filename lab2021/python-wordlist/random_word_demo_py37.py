6727e3e9eaba7cf96ff28848cb90b7ee5d12d637
smosley <smosley@uoregon.edu>
2021-08-12 08:17:54 -0700

    update-random-word-demo
    update-random-word-demo
    :cocktail: automated update ;; 20210812:081751
    
    	modified:   lab2021/python-wordlist/random_word_demo_py37.py

--- lab2021/python-wordlist/random_word_demo_py37.py
+++ lab2021/python-wordlist/random_word_demo_py37.py
@@ -19,8 +19,6 @@
 ###     seealso: |
 ###         ## See also
 ###         * capt="stacko question" ;; href="https://stackoverflow.com/questions/18834636/random-word-generator-python" ;; tags="__tags01__" ;; id="dmid://uu169stonk1628779x03xlink"
-###     seeinstead: |
-###         * __seeinstead__
 ### <end-file_info>
 '''
 
@@ -29,10 +27,16 @@
 
 ## <xrbeg id="uu439mospf1628779003aa"   d="python.init">##
 if(True):
+  import sys
   import re
   import pprint
+  import random
   pass
 ## <xrend>##uu439mospf1628779003aa
+
+# requires python py310
+# pprint.pprint(sys.stdlib_module_names)
+# exit();
 
 ## <xrbeg id="uu669smist1628779"        d="wordlist.init">##
 if(True):
@@ -45,11 +49,13 @@ if(True):
   vg33modules   = map(__import__, aa33listimpp)
   sg33doctext   = " ".join([vxx.__doc__ for vxx in vg33modules])
   pass
-  aa33listword  = [str(vxx).lower() for vxx in re.findall('[a-zA-Z0-9]{4,}',sg33doctext) ]
+  rgx33word4min = r'[a-zA-Z0-9]{4,}'  ## regex to find words of 4chars or more
+  aa33listword  = [str(vxx).lower() for vxx in re.findall(rgx33word4min,sg33doctext) ]
   aa33listword  = set(aa33listword)
   aa33listword  = sorted(aa33listword)
   pass
 
+  pprint.pprint(random.choice(aa33listword))
   pprint.pprint(aa33listword)
   pprint.pprint(aa33listword.__len__())
   pass
